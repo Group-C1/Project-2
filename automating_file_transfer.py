0d5be24a1abf2c92adc6d3f515ef91ad7547fb73
Aleksandra <amredichkina@gmail.com>
2023-01-23 16:06:04 +0100

    Task1(23.01.2023_v.2)_final

--- automating_file_transfer.py
+++ automating_file_transfer.py
@@ -23,6 +23,13 @@ from ftplib import FTP
 
 logging.basicConfig(filename='download.log', level=logging.INFO)
 
+host = input('Please, enter ftp server fost:\n')
+user = input('Please, enter user namet:\n')
+password = input('Please, enter password:\n')
+remote_dir = input('Please, enter directory address on ftp server:\n')
+local_dir = input('Please, enter directory address on the local computer:\n')
+destination = input('Please, enter the address on the internal network:\n ')
+
 
 def download_files(user, password, remote_dir, local_dir, destination):
     '''
@@ -33,47 +40,63 @@ def download_files(user, password, remote_dir, local_dir, destination):
     '''
 
     try:
+        # Connect to the FTP server
         ftp = FTP(host)
         ftp.login(user=user, passwd=password)
-        logging.info(f'The connection is established.')
+
+        # Log successful connection
+        logging.info('The connection is established.')
+
+        # Check if the local directory exists, if not create it
         if os.path.exists(local_dir) is not True:
             directory = 'Files'
             parent_dir = '/Users/Alexandra/Desktop/'
             path = os.path.join(parent_dir, directory)
             os.mkdir(path)
+
+        # Change the working directory on the FTP server
         ftp.cwd(remote_dir)
+
+        # Retrieve a list of files in the directory
         files = ftp.nlst()
-        for file_name in files:    
-            if ftp.size(file_name) <= 10485760:             #In order not to download very large files.
-                logging.info(f'Downloading... {file_name}')
-                ftp.retrbinary('RETR %s' %file_name,
-                               open(os.path.join(local_dir, file_name), 'wb').write)
+
+        # Iterate through the files, download and save to the local directory
+        for file_name in files:
+            logging.info(f'Downloading... {file_name}')
+            ftp.retrbinary('RETR %s' % file_name,
+                           open(os.path.join(local_dir, file_name), 'wb').write)
+        # Close the FTP connection
         ftp.close()
     except Exception as e:
         logging.error(f'Error connecting to FTP host: {e}')
 
-
     try:
+        # Moving downloaded files from a local directory to an internal network destination
         shutil.move(local_dir, destination)
         logging.info(f'{file_name} moved to {destination}')
     except Exception as e:
         logging.error(f'Error moving file: {e}')
 
-if __name__=='__main__':
-    host = 'ftp.otenet.gr'
-    user = 'speedtest'  
-    password = 'speedtest'
-    remote_dir ='/' 
-    local_dir = '/Users/Alexandra/Desktop/Files'
-    destination = '\\\\network\\files_for_sharing\\'
-    
-    schedule.every(20).seconds.do(download_files,
-                                 user=user,
-                                 password=password,
-                                 remote_dir=remote_dir,
-                                 local_dir=local_dir,
-                                 destination=destination)
-    logging.info('Script scheduled to run every 20 sec')
+
+def script_schedule(user, password, remote_dir, local_dir, destination):
+    '''
+    Script scheduled to run every day at 10:30.
+    '''
+    schedule.every().day.at("10:30").do(download_files,
+                                        user=user,
+                                        password=password,
+                                        remote_dir=remote_dir,
+                                        local_dir=local_dir,
+                                        destination=destination)
+    logging.info('Script is running...')
+
+
+def main():
+    script_schedule(user, password, remote_dir, local_dir, destination)
     while True:
-         schedule.run_pending()
-         time.sleep(1)
+        schedule.run_pending()
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
